b1c4444a801922506eaadc2d0000683bdff0fcd1
Lisa <lisaqdebona@gmail.com>
2024-09-25 01:21:29 -0400

    Styling Adventure Lodging

--- page-flexible-content.php
+++ page-flexible-content.php
@@ -262,7 +262,7 @@ $placeholder = THEMEURI . 'images/rectangle.png';
           $column1 = get_sub_field('column1');
           $column2 = get_sub_field('column2');
         ?>
-        <section id="section-fullwidth-black-<?php echo $i?>_parent<?php echo $ctr?>" data-section="<?php echo $e_title?>" class="mscol fullwidth section-fullwidth-black">
+        <section id="section-fullwidth-black-<?php echo $ctr?>" data-section="<?php echo $e_title?>" class="mscol fullwidth section-fullwidth-black">
           <div class="wrapper">
             <div class="flexwrap">
               <?php if ($column1) { ?>
@@ -283,6 +283,45 @@ $placeholder = THEMEURI . 'images/rectangle.png';
             </div>
           </div>
         </section>
+        <?php  } ?>
+
+
+        <?php if( get_row_layout() == 'middle_card' ) { 
+          $content_top = get_sub_field('content_top');
+          $content_bottom = get_sub_field('content_bottom');
+          $image = get_sub_field('image');
+          if($content_top || $content_bottom) { ?>
+          <section id="section-middle_card-<?php echo $ctr?>" class="fullwidth section-middle-card"> 
+            <div class="wrapper">
+              <div class="flexwrap">
+
+                <div class="fxcol left">
+                  <div class="inside">
+                    <?php if ($content_top) { ?>
+                    <div class="top"><?php echo $content_top ?></div>
+                    <?php } ?>
+
+                    <?php if ($content_bottom) { ?>
+                    <div class="bottom">
+                      <div class="wrap">
+                        <?php echo $content_bottom ?>
+                      </div>
+                    </div>
+                    <?php } ?>
+                  </div>
+                </div>
+
+                <?php if ($image) { ?>
+                  <div class="fxcol right">
+                    <figure>
+                      <img src="<?php echo $image['url'] ?>" alt="" />
+                    </figure>
+                  </div>
+                <?php } ?>
+              </div>
+            </div>
+          </section>
+          <?php  } ?>
         <?php  } ?>
 
       <?php $ctr++; endwhile;  ?>
