5fa3c3a77770fdab893022c5f2d65223c2cb97dc
Lisa <lisaqdebona@gmail.com>
2024-10-25 01:40:23 -0400

    Styling Race Series single post

---
+++ parts-flexible/simple_intro_text.php
@@ -0,0 +1,22 @@
+<?php if( get_row_layout() == 'simple_intro_text' ) { ?>
+
+  <?php 
+  $section_title = get_sub_field('title');
+  $section_text = get_sub_field('intro');
+  $data_section = ($section_title) ? ' data-section="'.$section_title.'"':'';
+  if( $section_title || $section_text ) { ?>
+  <div id="simple-intro-text-<?php echo $ctr ?>" class="simple_intro_text_section"<?php echo $data_section ?>>
+    <div class="midwrapper">
+      <?php if ($section_title ) { ?>
+        <div class="title-wrapper">
+          <h2 class="section-title"><?php echo $section_title ?></h2>
+        </div>
+      <?php } ?>
+        <?php if ($section_text) { ?>
+        <div class="section-text"><?php echo anti_email_spam($section_text) ?></div>
+        <?php } ?>
+    </div>
+  </div>
+  <?php } ?>
+
+<?php } ?>
--- single-race.php
+++ single-race.php
@@ -52,6 +52,7 @@ $show_faqs = get_field('show_faqs_items');
         <?php include( locate_template('parts-flexible/carousel_fullwidth.php') ); ?>
         <?php include( locate_template('parts-flexible/upcoming_events.php') ); ?>
         <?php include( locate_template('parts-flexible/schedule_cards.php') ); ?>
+        <?php include( locate_template('parts-flexible/simple_intro_text.php') ); ?>
 
       <?php $ctr++; endwhile;  ?>
     </div>
