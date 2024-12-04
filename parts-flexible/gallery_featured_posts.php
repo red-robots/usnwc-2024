a3042c197441045fbfd28395d419a8da45a62afb
Lisa <lisaqdebona@gmail.com>
2024-12-04 02:41:00 -0500

    Added new flexible content for Camps

--- parts-flexible/gallery_featured_posts.php
+++ parts-flexible/gallery_featured_posts.php
@@ -3,8 +3,9 @@
   <?php 
   $section_title = get_sub_field('section_title');
   $section_posts = get_sub_field('section_posts');
+  $data_section = ($section_title) ? ' data-section="'.$section_title.'"':'';
   if($section_posts) { ?>
-  <div id="section-gallery_featured_posts-<?php echo $ctr ?>" class="gallery_featured_posts section-upcoming_events">
+  <div id="section-gallery_featured_posts-<?php echo $ctr ?>"<?php echo $data_section ?> class="gallery_featured_posts fullwidth-float-left section-upcoming_events">
     <div class="wrapper">
       <?php if ($section_title) { ?>
       <h2 class="section-title"><?php echo $section_title ?></h2>
