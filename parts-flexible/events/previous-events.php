400db8000c50bb96013dfc2cf0859301e5d9908f
Lisa <lisaqdebona@gmail.com>
2024-12-11 02:56:49 -0500

    Made changes on Film Series page

---
+++ parts-flexible/events/previous-events.php
@@ -0,0 +1,104 @@
+<?php
+  $baseDate = date('Y-m-d', strtotime(' - 30 days'));
+  $args = array(
+    'posts_per_page'  => 4,
+    'post_type'       => $post_type,
+    'post_status'     => 'publish',
+    'meta_key' => 'start_date',
+    'orderby' => 'meta_value_num',
+    'order' => 'DESC',
+    'meta_query'      => array(
+      array(
+        'key' => 'start_date',
+        'value' => $baseDate,
+        'compare' => '<=',
+        'type' => 'DATE'
+      )
+    )
+  );
+  $posts = get_posts($args);
+  //$filtered_posts = array();
+  // if($posts) {
+  //   $max = 4; $i=1;
+  //   $reverse = array_reverse($posts);
+  //   for($i=0; $i<$max; $i++) {
+  //     $entry = $reverse[$i];
+  //     $filtered_posts[] = $entry;
+  //   }
+  // }
+
+  $filtered_posts = $posts;
+  $posts_by_month = array();
+  if($posts) {
+    $filtered_posts = array_reverse($posts);
+    foreach($filtered_posts as $p) {
+      $start_date = get_field('start_date', $p->ID);
+      if($start_date) {
+        $month = date('Ym', strtotime($start_date));
+        $posts_by_month[$month][] = $p; 
+      }
+    }
+  }
+
+  if($posts_by_month) { ?>
+  <?php foreach ($posts_by_month as $m=>$items) { 
+    $m = $m . '01';
+    $monthName = date('F Y',strtotime($m));
+    $monthSlug = strtolower( date('F-Y',strtotime($m)) );
+    $post_type = $items[0]->post_type;
+  ?>
+  <div id="section-upcoming_events-<?php echo $m ?>" class="repeatable-block section-upcoming_events monthly-events events--<?php echo $post_type ?>">
+    <div class="wrapper">
+      <h2 class="section-title"><?php echo $monthName ?></h2>
+
+      <div class="flexwrap">
+      <?php foreach ($items as $row) { 
+        $pid = $row->ID;
+        $title = $row->post_title;
+        $start_date = get_field('start_date', $pid);
+        $short_description = get_field('short_description', $pid);
+        $image = get_field('mobile-banner', $pid);
+        if(!$image) {
+          $image = get_field('thumbnail_image', $pid);
+        }
+        $pagelink = get_permalink($pid);
+        if($start_date) {
+          $start_date = date('l, F d', strtotime($start_date));
+        }
+      ?>
+      <div class="infobox">
+        <div class="inside">
+          <?php if ($start_date) { ?>
+          <div class="event-date"><?php echo $start_date ?></div>
+          <?php } ?>
+          <?php if ($image) { ?>
+          <figure class="event-image">
+            <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['url'] ?>" />
+          </figure>
+          <?php } else { ?>
+          <figure class="no-event-image"><span>Photo Not Available</span></figure>
+          <?php } ?>
+
+          <?php if ($title) { ?>
+          <h3 class="event-title"><?php echo $title ?></h3>
+          <?php } ?>
+
+          <?php if ($short_description) { ?>
+          <div class="summary"><?php echo $short_description ?></div>
+          <?php } ?>
+          
+          <div class="button-block">
+            <?php if ($post_type=='film-series') { ?>
+              <a href="javascript:void(0)" class="button-pill button-popup-details" data-postid="<?php echo $pid ?>">See Details</a>
+            <?php } else { ?>
+              <a href="<?php echo $pagelink ?>" class="button-pill">See Details</a>
+            <?php } ?>
+          </div>
+        </div>
+      </div>
+      <?php } ?>
+      </div>
+    </div>
+  </div>
+  <?php } ?>
+<?php } ?>
--- parts-flexible/weddings/upcoming_events_groupby_months.php
+++ parts-flexible/weddings/upcoming_events_groupby_months.php
@@ -3,97 +3,14 @@
   $btn = get_sub_field('button');
   $btnUrl = (isset($btn['url']) && $btn['url']) ? $btn['url'] : '';
   $btnTitle = (isset($btn['title']) && $btn['title']) ? $btn['title'] : '';
-  $btnTarget = (isset($btn['target']) && $btn['target']) ? $btn['target'] : '_self';
+  $btnTarget = (isset($btn['target']) && $btn['target']) ? $btn['target'] : '_self'; 
+
   if( $post_type ) { 
-    $args = array(
-      'posts_per_page'  => -1,
-      'post_type'       => $post_type,
-      'post_status'     => 'publish',
-      'meta_key' => 'start_date',
-      'orderby' => 'meta_value_num',
-      'order' => 'ASC',
-      'meta_query'      => array(
-        array(
-          'key' => 'start_date',
-          'value' => date('Y-m-d'),
-          'compare' => '>=',
-          'type' => 'DATE'
-        )
-      )
-    );
-    $posts = get_posts($args);
-    $posts_by_month = array();
-    if($posts) {
-      foreach($posts as $p) {
-        $start_date = get_field('start_date', $p->ID);
-        if($start_date) {
-          $month = date('Ym', strtotime($start_date));
-          $posts_by_month[$month][] = $p; 
-        }
-      }
+    if($post_type=='film-series') {
+      include( locate_template('parts-flexible/events/previous-events.php') );
     }
+    include( locate_template('parts-flexible/events/upcoming-by-month.php') );
+  }
 
-    if($posts_by_month) { ?>
-    <?php foreach ($posts_by_month as $m=>$items) { 
-      $m = $m . '01';
-      $monthName = date('F Y',strtotime($m));
-      $monthSlug = strtolower( date('F-Y',strtotime($m)) );
-      $post_type = $items[0]->post_type;
-    ?>
-    <div id="section-upcoming_events-<?php echo $m ?>" class="repeatable-block section-upcoming_events monthly-events events--<?php echo $post_type ?>">
-      <div class="wrapper">
-        <h2 class="section-title"><?php echo $monthName ?></h2>
-
-        <div class="flexwrap">
-        <?php foreach ($items as $row) { 
-          $pid = $row->ID;
-          $title = $row->post_title;
-          $start_date = get_field('start_date', $pid);
-          $short_description = get_field('short_description', $pid);
-          $image = get_field('mobile-banner', $pid);
-          if(!$image) {
-            $image = get_field('thumbnail_image', $pid);
-          }
-          $pagelink = get_permalink($pid);
-          if($start_date) {
-            $start_date = date('l, F d', strtotime($start_date));
-          }
-        ?>
-        <div class="infobox">
-          <div class="inside">
-            <?php if ($start_date) { ?>
-            <div class="event-date"><?php echo $start_date ?></div>
-            <?php } ?>
-            <?php if ($image) { ?>
-            <figure class="event-image">
-              <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['url'] ?>" />
-            </figure>
-            <?php } ?>
-            <?php if ($title) { ?>
-            <h3 class="event-title"><?php echo $title ?></h3>
-            <?php } ?>
-
-            <?php if ($short_description) { ?>
-            <div class="summary"><?php echo $short_description ?></div>
-            <?php } ?>
-            
-            <div class="button-block">
-              <?php if ($post_type=='film-series') { ?>
-                <a href="javascript:void(0)" class="button-pill button-popup-details" data-postid="<?php echo $pid ?>">See Details</a>
-              <?php } else { ?>
-                <a href="<?php echo $pagelink ?>" class="button-pill">See Details</a>
-              <?php } ?>
-            </div>
-          </div>
-        </div>
-        <?php } ?>
-        </div>
-      </div>
-    </div>
-    <?php } ?>
-    
-    <?php } ?>
-
-  <?php } ?>
-<?php }
+}
 
