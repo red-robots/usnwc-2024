e2ca9953a3737ae4984e3bce0151454c7ed66515
Lisa <lisaqdebona@gmail.com>
2024-12-19 00:54:14 -0500

    Fixed issues on Race posts

--- parts-calendar/calendar-events-featured.php
+++ parts-calendar/calendar-events-featured.php
@@ -68,11 +68,7 @@
   <div class="infoBox first post-type--<?php echo $post_type ?>">
     <div class="wrap">
       <figure>
-        <?php 
-        
-
-        $thumbnail = false;
-        if ($thumbnail) { ?>
+        <?php if ($thumbnail && isset($thumbnail['url'])) { ?>
           <img src="<?php echo $thumbnail['url'] ?>" alt="" />
         <?php } else { ?>
           <img src="<?php echo get_template_directory_uri(); ?>/images/image-not-available.jpg" alt="" />
