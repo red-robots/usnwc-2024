9b373a40a018af9cdc9a9be5cfb60961b99d3c32
Lisa <lisaqdebona@gmail.com>
2025-04-08 01:28:12 -0400

    Fixed Adventure Dining Hero section

--- parts/hero-red-tag.php
+++ parts/hero-red-tag.php
@@ -5,8 +5,9 @@ $registerLink = get_field('registrationLink');
 $regTarget = get_field('registrationLinkTarget');
 $status_custom_message = get_field('status_custom_message');
 $registerButton = 'Register';
-$registerTarget = ( isset($regTarget[0]) && $regTarget[0]=='yes' ) ? '_blank':'_self'; ?>
-<div class="hero-wrapper hero-register-button">
+$registerTarget = ( isset($regTarget[0]) && $regTarget[0]=='yes' ) ? '_blank':'_self'; 
+?>
+<div class="hero-wrapper hero-register-button" data-tag-status="<?php echo $status ?>">
 <?php if($status){ ?>
 	<?php get_template_part("parts/subpage-banner"); ?>
 	<?php if ($status=='open') { ?>
@@ -21,6 +22,20 @@ $registerTarget = ( isset($regTarget[0]) && $regTarget[0]=='yes' ) ? '_blank':'_
 		<div class="stats closed"><?php echo $status_custom_message ?></div>
 		<?php } ?>
 
-	<?php } ?>
+	<?php } else if($status=='custom_button') {  ?>
+
+    <?php if( $custom_button = get_field('status_custom_button', $post_id) ) { 
+      $statBtnName = (isset($custom_button['title']) && $custom_button['title']) ? $custom_button['title'] : '';
+      $statBtnUrl = (isset($custom_button['url']) && $custom_button['url']) ? $custom_button['url'] : '';
+      $statBtnTarget = (isset($custom_button['target']) && $custom_button['target']) ? $custom_button['target'] : '_self';
+      if($statBtnName && $statBtnUrl) { ?>
+      <div class="stats custom-button teaser">
+        <a href="<?php echo $statBtnUrl ?>" target="<?php echo $statBtnTarget ?>" class="registerBtn customButton"><?php echo $statBtnName ?></a>
+      </div>
+      <?php } ?>
+    <?php } ?>
+
+  <?php } ?>
+
 <?php } ?>
 </div>
