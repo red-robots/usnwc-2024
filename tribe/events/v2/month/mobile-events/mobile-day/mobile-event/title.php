d697afa9b91c48d712475c7742816bec7a7b3cd7
Lisa <lisaqdebona@gmail.com>
2025-05-22 01:32:12 -0400

    Fixed event link on mobile view

--- tribe/events/v2/month/mobile-events/mobile-day/mobile-event/title.php
+++ tribe/events/v2/month/mobile-events/mobile-day/mobile-event/title.php
@@ -17,18 +17,25 @@
  */
 
 $classes = [ 'tribe-events-calendar-month-mobile-events__mobile-event-title', 'tribe-common-h7' ];
-
+$tribeEventLink = tribe_get_event_website_url($event);
+$eventLink = ($tribeEventLink) ? $tribeEventLink : '';
 ?>
 <h3 <?php tribe_classes( $classes ); ?>>
-	<a
-		href="<?php echo esc_url( $event->permalink ); ?>"
-		title="<?php echo esc_attr( $event->title ); ?>"
-		rel="bookmark"
-		class="tribe-events-calendar-month-mobile-events__mobile-event-title-link tribe-common-anchor"
-	>
-		<?php
-		// phpcs:ignore
-		echo $event->title;
-		?>
-	</a>
+    <!-- <a
+      href="<?php //echo esc_url( $event->permalink ); ?>"
+      title="<?php //echo esc_attr( $event->title ); ?>"
+      rel="bookmark"
+      class="tribe-events-calendar-month-mobile-events__mobile-event-title-link tribe-common-anchor"
+      >
+      <?php
+      // phpcs:ignore
+      //echo $event->title;
+      ?>
+    </a> -->
+
+  <?php if ($tribeEventLink) { ?>
+  <a href="<?php echo $tribeEventLink ?>" title="<?php echo $event->title; ?>" rel="bookmark" class="tribe-events-calendar-month-mobile-events__mobile-event-title-link tribe-common-anchor eventName hasEventLink"><?php echo $event->title; ?></a>
+  <?php } else { ?>
+  <span class="mobile-event-title-nolink eventName noEventLink"><?php echo $event->title; ?></span>
+  <?php } ?>
 </h3>
