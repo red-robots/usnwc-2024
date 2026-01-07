c7caaec8eb6aa5557d1598f3a60f36b9a167564f
Lisa <lisaqdebona@gmail.com>
2026-01-06 23:59:02 -0500

    Added custom style in header.php

--- header.php
+++ header.php
@@ -55,6 +55,11 @@ var params={};location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(s,k,v){
 </script>
 <script>!function(e,t,n,s,a,c,p,i,o,u){e[a]||((i=e[a]=function(){i.process?i.process.apply(i,arguments):i.queue.push(arguments)}).queue=[],i.pixelId="cbd42ac9-c947-41a0-a340-cc2163106c8c",i.t=1*new Date,(o=t.createElement(n)).async=1,o.src="https://found.ee/dmp/pixel.js?t="+864e5*Math.ceil(new Date/864e5),(u=t.getElementsByTagName(n)[0]).parentNode.insertBefore(o,u))}(window,document,"script",0,"foundee");foundee('', 'Y');</script>
 <?php wp_head(); ?>
+<style>
+.site:has(#banner .iframe-wrapper video) .site-content {
+  background-color: #FFF;
+}
+</style>
 </head>
 <?php
 $heroImage = get_field("full_image");
