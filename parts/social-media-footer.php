bfad4f7395563ff67ee773a35c47e6cbfda8d6ce
Lisa <lisaqdebona@gmail.com>
2026-01-27 00:54:26 -0500

    Made changes on footer social media

--- parts/social-media-footer.php
+++ parts/social-media-footer.php
@@ -24,8 +24,12 @@
 				$social_icon = $m['icon']; 
         if($social_link && $social_icon) {
           $socialName = get_root_name_from_url($social_link);
-          ?>
-				  <a href="<?php echo $social_link ?>" target="_blank" class="social-icon-<?php echo $socialName ?>"><i class="<?php echo $social_icon ?>"></i><span class="sr-only"><?php echo ucwords($socialName) ?></span></a>	
+          $icon = get_field( strtolower($socialName).'_icon','option');
+          if($icon) { ?>
+				  <a href="<?php echo $social_link ?>" target="_blank" class="social-icon-<?php echo $socialName ?>"><span class="sr-only"><?php echo ucwords($socialName) ?></span>
+            <img src="<?php echo $icon ?>" alt="<?php echo ucwords($socialName) ?> icon" />
+          </a>
+          <?php } ?>	
         <?php } ?>
 			<?php
 			}	
